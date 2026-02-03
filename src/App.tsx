5a9d193bbf6cfa9fb8e7aa24e93e14e0218354ce
ILoharces <ibai.loharces@jakala.com>
2026-02-03 10:46:20 +0100

    feat: mensaje de cargando cuando la lista sea undefined

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import { PokeItem } from "./components/pokeItem";
 import { usePokemon } from "./hooks/usePokemon";
-import type { PokemonListItem } from "./types/Pokemon";
+import type { PokemonListItem } from "./types/PokemonUtils";
 
 function App() {
-  const [selectedPokemonName, setSelectedPokemonName] = useState<string | null>(null);
-  const {pokemonList, pokemon} = usePokemon(selectedPokemonName??undefined);
-  
+  const [selectedPokemonName, setSelectedPokemonName] = useState<string | undefined>(undefined);
+  const { pokemonList, pokemon } = usePokemon(selectedPokemonName ?? undefined);
+
   return (
-    <div className="flex gap-4">
-      <div className="flex-1">
-        {pokemonList?.map((p: PokemonListItem) => (
-          <PokeItem key={p.id} name={p.name} setSelectedPokemonName={setSelectedPokemonName} />
-        ))}
+    <div className="flex gap-4 mt-4 ml-4 mr-4 mb-4 p-4 border-2 rounded-2xl">
+      <div className="flex-1 ml-4 border-2 p-4">
+        {pokemonList ?
+          pokemonList.map((p: PokemonListItem) => (
+            <PokeItem key={p.id} name={p.name} setSelectedPokemonName={setSelectedPokemonName} />
+          ))
+          : <p>cargando...</p>
+        }
+
       </div>
-      <div className="flex-1">
-        {pokemon && <div>{pokemon.name}</div>}
+      <div className="flex-1 mr-4 border-2 border-r p-4">
+        {pokemon && <img src={pokemon.sprites.front_default ?? ''}></img>}
       </div>
     </div>
   );
